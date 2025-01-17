884cd437f60f6f7f22566001563f74baa0f6819b
unknown <vanquyd647@gmail.com>
2025-01-17 18:00:57 +0700

    add index 6h   2 17_1_2025

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../store/slices/userSlice';
 import productReducer from '../store/slices/productSlice';
 import cartReducer from '../store/slices/cartSlice';
-import reviewsReducer from '../store/slices/reviewsSlice';
+import reviewsReducer from '../store/slices/reviewsSlice';  
 
 const store = configureStore({
     reducer: {
