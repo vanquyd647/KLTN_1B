c2c4fe78d86e1fbe4b5724259cbfa504fe321ce9
unknown <vanquyd647@gmail.com>
2025-05-27 18:22:26 +0700

    update css 27_5_2025

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,6 +12,14 @@ export default function App({ Component, pageProps }) {
                     rel="icon"
                     href="https://firebasestorage.googleapis.com/v0/b/red89-f8933.appspot.com/o/KLTN%2FDALL%C2%B7E%202025-01-20%2014.48.34%20-%20A%20minimalistic%20logo%20for%20a%20Fashion%20Store%2C%20designed%20for%20use%20as%20a%20website%20favicon.%20The%20logo%20features%20a%20sleek%20and%20modern%20design%20with%20an%20abstract%20represent.webp?alt=media&token=da1ff9d9-3a6e-44ca-ba4c-6de1a45b80fc"
                 />
+                <meta name="description" content="Mua sắm thời trang với các sản phẩm mới nhất và phong cách nhất tại Fashion Store." />
+                <meta name="keywords" content="thời trang, mua sắm, sản phẩm mới, quần áo, giày dép" />
+                <meta name="author" content="Fashion Store" />
+                <meta property="og:title" content="Fashion Store" />
+                <meta property="og:description" content="Mua sắm thời trang với các sản phẩm mới nhất và phong cách nhất." />
+                <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/red89-f8933.appspot.com/o/KLTN%2FDALL%C2%B7E%202025-01-20%2014.48.34%20-%20A%20minimalistic%20logo%20for%20a%20Fashion%20Store%2C%20designed%20for%20use%20as%20a%20website%20favicon.%20The%20logo%20features%20a%20sleek%20and%20modern%20design%20with%20an%20abstract%20represent.webp?alt=media&token=da1ff9d9-3a6e-44ca-ba4c-6de1a45b80fc" />
+                <meta property="og:url" content="https://kltn-1-b.vercel.app/" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Component {...pageProps} />
         </Provider>
